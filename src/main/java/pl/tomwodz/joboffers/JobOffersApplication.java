b8fd3e673928b2a3af011c1e6cb02979d3c9ac8c
twodzinski@op.pl <twodzinski@op.pl>
2023-09-18 08:30:19 +0200

    added spring boot

---
+++ src/main/java/pl/tomwodz/joboffers/JobOffersApplication.java
@@ -0,0 +1,11 @@
+package pl.tomwodz.joboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersApplication.class);
+    }
+}
