bd389a029d7020539d7f4637631ddba9e250d1d1
twodzinski@op.pl <twodzinski@op.pl>
2023-09-19 10:22:12 +0200

    added configuration properties

--- src/main/java/pl/tomwodz/joboffers/JobOffersApplication.java
+++ src/main/java/pl/tomwodz/joboffers/JobOffersApplication.java
@@ -2,8 +2,11 @@ package pl.tomwodz.joboffers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.tomwodz.joboffers.infrastructure.clientoffer.ClientOfferRestTemplateConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ClientOfferRestTemplateConfigurationProperties.class})
 public class JobOffersApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobOffersApplication.class);
---
+++ src/main/java/pl/tomwodz/joboffers/infrastructure/clientoffer/ClientOfferRestTemplateConfigurationProperties.java
@@ -0,0 +1,13 @@
+package pl.tomwodz.joboffers.infrastructure.clientoffer;
+
+import lombok.Builder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "job-offers.client-offer.http.client.config")
+@Builder
+public record ClientOfferRestTemplateConfigurationProperties(
+        String uri,
+        int port,
+        int connectionTimeout,
+        int readTimeout) {
+}
